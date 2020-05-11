fe4890a485f5f40c791dc614b30deb3347a0ea5e
d-kfmnn <59046237+d-kfmnn@users.noreply.github.com>
2020-05-11 12:56:48 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -7,16 +7,20 @@ echo "substitution" > btor64.log
 echo "verify" >> btor64.log
 ../amulet -verify btor64_rewr.aig  >> btor64.log
 
+# Verifying a faulty unsigned multiplier
+echo "substitution" > btor2faulty.log
+../amulet -substitute btor2faulty.aig btor2faulty.cnf btor2faulty_rewr.aig >> btor2faulty.log
+echo "verify" >> btor2faulty.log
+../amulet -verify btor2faulty_rewr.aig  >> btor2faulty.log
+
 # Verifying a complex unsigned multiplier
 echo "substitution" > bpwtcl64.log
 ../amulet -substitute bpwtcl64.aig bpwtcl64.cnf bpwtcl64_rewr.aig >> bpwtcl64.log
 echo "verify" >> bpwtcl64.log
 ../amulet -verify bpwtcl64_rewr.aig  >> bpwtcl64.log
 
-
 # Certifying a complex signed multiplier
 echo "substitution" > s_sparcl64.log
 ../amulet -substitute s_sparcl64.aig s_sparcl64.cnf s_sparcl64_rewr.aig -2comp >> s_sparcl64.log
 echo "certify" >> s_sparcl64.log
 ../amulet -certify -2comp s_sparcl64_rewr.aig s_sparcl64.polys s_sparcl64.pac s_sparcl64.spec >> s_sparcl64.log
-
